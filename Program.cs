ca3738ddec5f10d37983d3698c58476d15d621f4
@eveertonrs <evertonbarbosars@gmail.com>
2023-01-29 13:06:52 -0200

    CRUD basico, utilizando endpoints para manipular produtos, metodos GET,POST,PUT,DELETE

--- Program.cs
+++ Program.cs
@@ -1,16 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! 4");
-app.MapGet("/user", () => "Everton Barbosa");
-app.MapPost("/user", () => new {Name = "Everton barbosa", Age = 26});
+//Salva o produto em memoria
+app.MapPost("/salvarproduto", (Produto produto) => {
+    RepositorioProduto.Add(produto);
+});
 
-app.MapPost("/salvar", (Produto produto) => {
-    return produto.Codigo +  " - " + produto.Nome;
+//api.app.com/user/{code}
+//Pega o produto na memoria por ID
+app.MapGet("/getproduto/{codigo}", ([FromRoute] string codigo) => {
+    var produto = RepositorioProduto.Getby(codigo);
+    return produto;
+});
+
+app.MapGet("/getprodutobyheader", (HttpRequest request) => {
+    return request.Headers["codigo-produto"].ToString();
+});
+
+//Edita o produto
+app.MapPut("/editarproduto",(Produto produto) => {
+    //Busca o produto salvo pelo o ID
+    var produtosalvo = RepositorioProduto.Getby(produto.Codigo);
+    //edita a variavel nome
+    produtosalvo.Nome = produto.Nome;
+});
+
+app.MapDelete("/deletarproduto/{codigo}", ([FromRoute] string codigo) => {
+     var produtosalvo = RepositorioProduto.Getby(codigo);
+     RepositorioProduto.Remove(produtosalvo);
 });
 
 app.Run();
 
+
+//Repositorio para salvar dados em memoria sem banco de dados
+// Lista para salvar os produtos e função para adicionar a essa lista
+public class  RepositorioProduto{
+    public static List<Produto> Produtos { get; set; }
+
+    //Adiciona o produto na Lista produto
+    public static void Add(Produto produto){
+        if(Produtos == null)
+            Produtos = new List<Produto>();
+
+        Produtos.Add(produto);
+    }
+
+    public static Produto Getby(string codigo){
+        return Produtos.FirstOrDefault(p => p.Codigo == codigo);
+    }
+    public static void Remove(Produto produto){
+        Produtos.Remove(produto);
+    }
+}
 
 public class Produto { 
     public string Codigo { get; set; }
